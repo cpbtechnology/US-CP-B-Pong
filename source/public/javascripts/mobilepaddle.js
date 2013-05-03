c831de81dfbc35d70cdb05a6667f14ae5a99504b
Pablo Alejo <toe@hotsaucestudio.com>
2013-05-03 14:21:20 -0600

    Added the ability to control paddle with touch

--- source/public/javascripts/mobilepaddle.js
+++ source/public/javascripts/mobilepaddle.js
@@ -28,30 +28,39 @@
 	var beta;
 	var paddlePos = 400;
 	var self = this;
-	startPaddle = function(){
+	startPaddle = function(){ 
+		// Uses the phone acceleramater 
+		/*
 		window.ondevicemotion = function(event) {
 			beta = Math.round(event.accelerationIncludingGravity.y*100)/100;
 			
 			if (beta > .6){
 				paddlePos = paddlePos+(beta*beta);
-		
 			}
 			if (beta < -.3){
 				paddlePos = paddlePos-(-beta*(-beta));
-		
 			}
 			if (paddlePos <= 0){
 				paddlePos = 0;
-				
 			}
 			if (paddlePos >= 355){
 				paddlePos = 355;
-				
 			}
 			socket.emit('paddleLocation', {paddlePos: paddlePos, MobilePlayer: MobilePlayer});
-			
-
 		}
+		*/
+
+		window.addEventListener('touchmove', function(event){
+			event.preventDefault();
+    		var touch = event.touches[0];
+    		var paddlePos = (touch.pageY / $(window).height() * 355);
+    		$('#paddlePosition').html(paddlePos);
+    		//$('#paddlePosition').html(touch.pageX + " - " + touch.pageY + " Height - " + $(window).height());
+    		socket.emit('paddleLocation', {paddlePos: paddlePos, MobilePlayer: MobilePlayer});
+    		
+
+
+		}, false);
 	}
 
 	
