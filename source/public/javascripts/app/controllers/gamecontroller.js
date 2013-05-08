7d7a377e531731e690becaf7ca24df6da66b9661
Pablo Alejo <toe@hotsaucestudio.com>
2013-05-08 16:42:55 -0600

    adding style

--- source/public/javascripts/app/controllers/gamecontroller.js
+++ source/public/javascripts/app/controllers/gamecontroller.js
@@ -31,9 +31,10 @@
 		    var self = this; // Self points to controller
 		    
 		    RoomModel.findOne({id: data.room_id}, function(room){
-			
+				var aryQueryString = self.getQueryString();
+								
 				self.room = room;	
-				if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+				if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || aryQueryString.mobile == "true") {
 					self.element.html(Templates["pages/partial.mobileroom.jade"]);
 				}	
 				else {		
@@ -50,8 +51,28 @@
 
 				console.log('joined room: '+ room.title);
 			});
-			
-			
+	    },
+	    getQueryString: function(){
+	    	// This function is anonymous, is executed immediately and 
+			// the return value is assigned to QueryString!
+			var query_string = {};
+			var query = window.location.search.substring(1);
+			var vars = query.split("&");
+			for (var i=0;i<vars.length;i++) {
+				var pair = vars[i].split("=");
+				// If first entry with this name
+				if (typeof query_string[pair[0]] === "undefined") {
+				  query_string[pair[0]] = pair[1];
+					// If second entry with this name
+				} else if (typeof query_string[pair[0]] === "string") {
+				  var arr = [ query_string[pair[0]], pair[1] ];
+				  query_string[pair[0]] = arr;
+					// If third or later entry with this name
+				} else {
+				  query_string[pair[0]].push(pair[1]);
+				}
+			} 
+			return query_string;
 	    }
 	
 	});
