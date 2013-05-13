34ce61f0b1e3feea24a3676999b5ced89deadd69
Benton <bentonrochester@gmail.com>
2013-05-13 15:43:49 -0600

    fixed rooms controller

--- source/public/javascripts/app/controllers/gamecontroller.js
+++ source/public/javascripts/app/controllers/gamecontroller.js
@@ -21,14 +21,9 @@
 	      },
 		"route": function(data, room){
 			var self=this;
-		    	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || aryQueryString.mobile == "true") {
-					self.element.html(Templates["pages/partial.mobileroom.jade"]);
-				}	
-				else {		
-					self.element.html(Templates["pages/partial.room.jade"]);
-					//self.element.html(Templates["pages/partial.mobileroom.jade"]); 
-				}
-		      
+		      RoomModel.findAll({}, function(rooms){ //.findAll is an ajax call to the back end
+		        self.element.html(Templates["pages/partial.rooms.jade"]({rooms:rooms})); //renders html partial.rooms.jade
+		      });
 	
 		},
 		 ":room_id route": function(data, room) {
